ac60890e8c616932daf98654631d31894572e2b6
i-panda7 <i_panda7@outlook.com>
2024-06-08 14:00:38 +0530

    11

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-// Import the express module
 const express = require('express');
-
-// Create an instance of the express application
 const app = express();
+const path = require('path');
 
-// Define the port to run the server on
-const PORT = 3000;
+app.use(express.static(path.join(__dirname, 'web')));
 
-// Use express to serve static files from the 'web' directory
-app.use(express.static('web'));
-
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/web/index.html');
+app.get('/welcome', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web', 'index.html'));
 });
 
-// Start the server
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web', 'password.html'));
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web', 'home.html'));
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
