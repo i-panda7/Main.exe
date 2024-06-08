0607abf12e7e74a5b2ce6c9dc7a8d0d9a45a0ea9
i-panda7 <i_panda7@outlook.com>
2024-06-08 14:05:11 +0530

    111

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config();
 
 app.use(express.static(path.join(__dirname, 'web')));
+app.use(express.json());
 
 app.get('/welcome', (req, res) => {
     res.sendFile(path.join(__dirname, 'web', 'index.html'));
@@ -10,6 +12,15 @@ app.get('/welcome', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'web', 'password.html'));
+});
+
+app.post('/check-password', (req, res) => {
+    const { password } = req.body;
+    if (password === process.env.passwordweb) {
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
 });
 
 app.get('/home', (req, res) => {
