fb1128514d8277c931676706eaa651393b7fbcfd
i-panda7 <i_panda7@outlook.com>
2024-06-08 14:26:45 +0530

    2

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-require('dotenv').config();
+const discord = require('./Discord/discord.js')
+const Discord = require('discord.js')
+Discord(discord)
+console.log("Starting Module :: ✅ Discord ")
 
-app.use(express.static(path.join(__dirname, 'web')));
-app.use(express.json());
-
-app.get('/welcome', (req, res) => {
-    res.sendFile(path.join(__dirname, 'web', 'index.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'web', 'password.html'));
-});
-
-app.post('/check-password', (req, res) => {
-    const { password } = req.body;
-    if (password === process.env.passwordweb) {
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
-    }
-});
-
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'web', 'home.html'));
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const express = require("./website/express.js")
+const Express = require("express.js")
+Express(express)
+console.log("Starting Module :: ✅ Express ")
